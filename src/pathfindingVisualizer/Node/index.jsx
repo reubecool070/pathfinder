52c96eceadd9a915d1d1efd3a96f0a8c3cf0aa38
reubecool070 <reubenthapa070@gmail.com>
2024-07-01 20:59:10 +0545

    refactor: props validation

--- src/pathfindingVisualizer/Node/index.jsx
+++ src/pathfindingVisualizer/Node/index.jsx
@@ -1,12 +1,37 @@
+import PropTypes from "prop-types";
 import "./index.css";
 
 // eslint-disable-next-line react/prop-types
-const Node = ({ isFinish, isStart, row, col }) => {
+const Node = ({
+  isFinish,
+  isStart,
+  row,
+  col,
+  onMouseDown,
+  onMouseUp,
+  onMouseEnter,
+}) => {
   const nodeClassName = isFinish ? "finish" : isStart ? "start" : "";
 
   return (
-    <div id={`node-${row}-${col}`} className={`node ${nodeClassName}`}></div>
+    <div
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseUp={() => onMouseUp(row, col)}
+      onMouseEnter={() => onMouseEnter(row, col)}
+      id={`node-${row}-${col}`}
+      className={`node ${nodeClassName}`}
+    ></div>
   );
 };
 
 export default Node;
+
+Node.propTypes = {
+  isFinish: PropTypes.bool.isRequired,
+  isStart: PropTypes.bool.isRequired,
+  row: PropTypes.number.isRequired,
+  col: PropTypes.number.isRequired,
+  onMouseDown: PropTypes.func.isRequired,
+  onMouseUp: PropTypes.func.isRequired,
+  onMouseEnter: PropTypes.func.isRequired,
+};
