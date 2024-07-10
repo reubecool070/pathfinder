9d55c69258b17f1c13ff63965706da029b00dee3
reubecool070 <reubenthapa070@gmail.com>
2024-07-10 22:50:35 +0545

    feat: add astar algorithm

--- src/data/constant.js
+++ src/data/constant.js
@@ -1,7 +1,7 @@
 export const START_NODE_ROW = 10;
 export const START_NODE_COLUMN = 15;
-export const FINISH_NODE_ROW = 10;
-export const FINISH_NODE_COLUMN = 35;
+export const FINISH_NODE_ROW = 6;
+export const FINISH_NODE_COLUMN = 20;
 
 export const ROW_COUNT = 15;
 export const COLUMN_COUNT = 50;
